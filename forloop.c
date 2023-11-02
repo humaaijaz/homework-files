f7af0845b9e262dcd28b64c595bc253c810c69da
humaaijaz <146563055+humaaijaz@users.noreply.github.com>
2023-11-02 12:54:52 +0530

    Add files via upload

---
+++ forloop.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int count=0;
+    for(int i=1;i<=100;i++) {
+        if(i%2 !=0 && i%3 !=0 && i%5 !=0) {
+            count++;
+
+        }
+        
+        
+    }
+    printf("%d\n", count);
+    return 0;
+}
+
